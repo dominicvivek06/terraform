317d1d7c36ba836dd5a59342b9eb1488052c539c
Dominic KUMAR <dominicvivek06@gmail.com>
2022-10-31 22:48:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,3 +73,32 @@ resource "azurerm_network_interface" "app_interface" {
     azurerm_public_ip.app_public_ip
   ]
 }
+
+
+
+resource "azurerm_windows_virtual_machine" "app_vm" {
+  name                = "appvm"
+  resource_group_name = local.resource_group
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "demousr"
+  admin_password      = "Azure@123"
+  network_interface_ids = [
+    azurerm_network_interface.app_interface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  depends_on = [
+    azurerm_network_interface.app_interface
+  ]
+}
