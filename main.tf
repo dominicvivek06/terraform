58b8e8aeeb00a11d9efc42f6ba5f247a83eaa5af
Dominic KUMAR <dominicvivek06@gmail.com>
2022-10-31 22:47:04 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+    subscription_id = "a14c-3a36c623e5ed"
+    client_id = "e81c72dc90d4"
+    client_secret = "fVyQ7SZjI9iBpctb"
+    tenant_id = "e45b9887cbdd"
+    features {    }
+    
+}
